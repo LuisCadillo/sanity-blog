0775c90f36ebfef92002f2b41c89fd3347549c45
LuisCadillo <luiscaf3r@gmail.com>
2022-07-14 17:13:25 -0500

    FEATURE: Nextjs preview mode

--- components/avatar.js
+++ components/avatar.js
@@ -1,18 +1,20 @@
-import Image from 'next/image'
-import { urlForImage } from '../lib/sanity'
+import Image from "next/image";
+import { urlForImage } from "../lib/sanity";
 
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <div className="w-12 h-12 relative mr-4">
+      <div className="relative w-12 h-12 mr-4">
+        {/*
         <Image
-          src={urlForImage(picture).height(96).width(96).fit('crop').url()}
+          // src={urlForImage(picture).height(96).width(96).fit('crop').url()}
           layout="fill"
           className="rounded-full"
           alt={name}
         />
+        */}
       </div>
       <div className="text-xl font-bold">{name}</div>
     </div>
-  )
+  );
 }
