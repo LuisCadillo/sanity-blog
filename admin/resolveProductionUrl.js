d664fed60e0c297a529432336e34a4f976cad818
LuisCadillo <luiscaf3r@gmail.com>
2022-07-14 23:29:39 -0500

    FEATURE: Add live preview on sanity studio

--- admin/resolveProductionUrl.js
+++ admin/resolveProductionUrl.js
@@ -1,6 +1,19 @@
-const previewSecret = process.env.SANITY_PREVIEW_SECRET // Copy the string you used for SANITY_PREVIEW_SECRET
-const projectUrl = 'http://localhost:3000'
+// Any random string, must match SANITY_PREVIEW_SECRET in the Next.js .env.local file
+const previewSecret = "TxSm5KHvkgd102VpSlmK3";
 
-export default function resolveProductionUrl(document) {
-  return `${projectUrl}/api/preview?secret=${previewSecret}&slug=${document.slug.current}`
+// Replace `remoteUrl` with your deployed Next.js site
+const remoteUrl = `https://cms-sanity-six-rosy.vercel.app`;
+const localUrl = `http://localhost:3000`;
+
+export default function resolveProductionUrl(doc) {
+  const baseUrl =
+    window.location.hostname === "localhost" ? localUrl : remoteUrl;
+
+  const previewUrl = new URL(baseUrl);
+
+  previewUrl.pathname = `/api/preview`;
+  previewUrl.searchParams.append(`secret`, previewSecret);
+  previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`);
+
+  return previewUrl.toString();
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+const STUDIO_REWRITE = {
+  source: "/admin/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/admin/:path*"
+      : "/admin/index.html",
+};
+
 module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+  nextConfig,
   images: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
   },
-}
+};
